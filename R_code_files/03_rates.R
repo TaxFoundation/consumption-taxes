e0f29760c584a480b4955d4e76477538bfab9ea2
Daniel Bunn <dbunn@taxfoundation.org>
2020-12-04 14:15:59 -0500

    US and Canadian equivalent VAT rates

--- R_code_files/03_rates.R
+++ R_code_files/03_rates.R
@@ -63,25 +63,21 @@ colnames(vat_rate_avg)[colnames(vat_rate_avg)=="n"] <- "observations"
 write.csv(vat_rate_avg, paste(intermediate_outputs,"vat_rate_avg.csv",sep=""), row.names = F)
 
 #US VAT rate equivalent
-columns <- names(vat_rates)
-values <- c("United States","","7.2","7.3","7.3","7.4","7.4","7.4","7.4")
-US <- data.frame(columns, values)
-US <- spread(US, columns, values)
+vat_rates<-vat_rates[-c(4)]
 
-vat_rates <- rbind(vat_rates, US)
+year<- c("2014","2015","2016","2017","2018","2019","2020")
+rate<- c("7.2","7.3","7.3","7.4","7.4","7.4","7.4")
+country<-c("United States","United States","United States","United States","United States","United States","United States")
+us <- data.frame(country,year, rate)
+
+write.csv(us, paste(intermediate_outputs,"us_vat_equivalent.csv",sep=""), row.names = F)
+
 
 #Canada VAT rate equivalent - https://www.retailcouncil.org/resources/quick-facts/sales-tax-rates-by-province/
-columns <- names(vat_rates)
-values <- c("Canada","","15.6","10.6","10.6","12.4","12.4","12.4","12.4")
-Canada <- data.frame(columns, values)
-Canada <- spread(Canada, columns, values)
 
-vat_rates <- subset(vat_rates, vat_rates$country!="Canada*")
-vat_rates <- rbind(vat_rates, Canada)
+year<- c("2014","2015","2016","2017","2018","2019","2020")
+rate<- c("15.6","10.6","10.6","12.4","12.4","12.4","12.4")
+country<-c("Canada","Canada","Canada","Canada","Canada","Canada","Canada")
+canada <- data.frame(country,year, rate)
 
-vat_rates <- vat_rates[-c(2)]
-vat_rates <- melt(vat_rates,id.vars=c("country"))
-colnames(vat_rates) <- c("country", "year", "vat_rate")
-vat_rates$country <- str_remove_all(vat_rates$country, "[*]")
-
-write.csv(vat_rates,paste(intermediate_outputs,"vat_rates.csv",sep=""),row.names = FALSE)
+write.csv(canada, paste(intermediate_outputs,"canada_vat_equivalent.csv",sep=""), row.names = F)
