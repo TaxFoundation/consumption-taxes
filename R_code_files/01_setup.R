12686c443c65d528d717ca6965132d5cd76cde81
Daniel Bunn <dbunn@taxfoundation.org>
2020-12-09 10:34:05 -0500

    fixing revenue r code
    prepping for matching up with other datasets

--- R_code_files/01_setup.R
+++ R_code_files/01_setup.R
@@ -2,7 +2,7 @@
 R_code_files<-"C:/Github/consumption-taxes/R_code_files/"
 source_data<-"C:/Github/consumption-taxes/source_data/"
 intermediate_outputs<-"C:/Github/consumption-taxes/intermediate_outputs/"
-revenue<-"C:/Github/consumption-taxes/intermediate_outputs/revenue/"
+revenues<-"C:/Github/consumption-taxes/intermediate_outputs/revenue/"
 rates<-"C:/Github/consumption-taxes/intermediate_outputs/rates/"
 thresholds<-"C:/Github/consumption-taxes/intermediate_outputs/thresholds/"
 base<-"C:/Github/consumption-taxes/intermediate_outputs/base/"
--- R_code_files/02_revenue.R
+++ R_code_files/02_revenue.R
@@ -13,48 +13,45 @@ dataset <- ("REV")
 #dstruc$YEA
 taxes<-c("5000","5111","5112","5121")
 
-data <- get_dataset(dataset, filter= list(c("NES"),c(taxes),c("TAXGDP","TAXPER"),c(oecd_countries)),start_time = 2000)
+revenue <- get_dataset(dataset, filter= list(c("NES"),c(taxes),c("TAXGDP","TAXPER"),c(oecd_countries)),start_time = 1975)
 
 #Drop redundant columns
-data <- subset(data, select=-c(GOV, TIME_FORMAT,POWERCODE,UNIT))
+revenue <- subset(revenue, select=-c(GOV, TIME_FORMAT,POWERCODE,UNIT))
 
 #Rename columns
-colnames(data)[colnames(data)=="COU"] <- "iso_3"
-colnames(data)[colnames(data)=="TAX"] <- "tax"
-colnames(data)[colnames(data)=="VAR"] <- "variable"
-colnames(data)[colnames(data)=="obsTime"] <- "year"
-colnames(data)[colnames(data)=="obsValue"] <- "value"
+colnames(revenue)[colnames(revenue)=="COU"] <- "iso_3"
+colnames(revenue)[colnames(revenue)=="TAX"] <- "tax"
+colnames(revenue)[colnames(revenue)=="VAR"] <- "variable"
+colnames(revenue)[colnames(revenue)=="obsTime"] <- "year"
+colnames(revenue)[colnames(revenue)=="obsValue"] <- "value"
 
 #Add country names and continents to data, and add variable signaling OECD countries####
-data <- merge(data, country_names, by='iso_3')
-
-#Adjust the order of the columns
-data <- data[c("iso_2", "iso_3", "country", "continent", "year", "tax","variable", "value")]
+revenue <- merge(revenue, country_names, by='iso_3')
 
 #Fix country name that was read in incorrectly
-data$country <- as.character(data$country)
+revenue$country <- as.character(revenue$country)
 
 #Rename Tax Codes
-data$tax<-if_else(data$tax=="5000","5000 Consumption",data$tax)
-data$tax<-if_else(data$tax=="5111","5111 VAT",data$tax)
-data$tax<-if_else(data$tax=="5112","5112 Sales",data$tax)
-data$tax<-if_else(data$tax=="5121","5121 Excise",data$tax)
+revenue$tax<-if_else(revenue$tax=="5000","5000 Consumption",revenue$tax)
+revenue$tax<-if_else(revenue$tax=="5111","5111 VAT",revenue$tax)
+revenue$tax<-if_else(revenue$tax=="5112","5112 Sales",revenue$tax)
+revenue$tax<-if_else(revenue$tax=="5121","5121 Excise",revenue$tax)
 
 #Rename Variable Codes
-data$variable<-if_else(data$variable=="TAXGDP","% of GDP",data$variable)
-data$variable<-if_else(data$variable=="TAXPER","% of Total Revenue",data$variable)
+revenue$variable<-if_else(revenue$variable=="TAXGDP","% of GDP",revenue$variable)
+revenue$variable<-if_else(revenue$variable=="TAXPER","% of Total Revenue",revenue$variable)
 
-write.csv(data, paste(revenue,"revenue_preliminary.csv",sep=""), row.names = F)
+write.csv(revenue, paste(revenues,"revenue_preliminary.csv",sep=""), row.names = F)
 
 #Fix countries for which 2019 data is not available (unless otherwise noted, 2018 data is used for these cases)####
 
 #Australia: 2019 data not available -> use 2018 data
-missing_australia <- data
+missing_australia <- revenue
 missing_australia <- subset(missing_australia, subset = iso_3 == "AUS" & year == "2018")
 missing_australia[missing_australia$year == 2018, "year"] <- 2019
 
 #Greece: 2019 data not available for VAT and Excise -> use 2018 data for VAT and Excise
-missing_greece <- data
+missing_greece <- revenue
 missing_greece_vat <- subset(missing_greece, subset = iso_3 == "GRC" & year == "2018" & tax == "5111 VAT")
 missing_greece_excise <- subset(missing_greece, subset = iso_3 == "GRC" & year == "2018" & tax == "5121 Excise")
 
@@ -62,7 +59,7 @@ missing_greece_vat[missing_greece_vat$year == 2018, "year"] <- 2019
 missing_greece_excise[missing_greece_excise$year == 2018, "year"] <- 2019
 
 #Japan: 2017, 2018, and 2019 data not available for Sales-> use 2016 data for Sales; 2019 % of revenue not available for excise, VAT, and consumption -> Use 2018
-missing_japan <- data
+missing_japan <- revenue
 missing_japan_sales <- subset(missing_japan, subset = iso_3 == "JPN" & year == "2016" & tax == "5112 Sales")
 missing_japan_sales_2017<-missing_japan_sales
 missing_japan_sales_2018<-missing_japan_sales
@@ -78,68 +75,117 @@ missing_japan_2019[missing_japan_2019$year == 2018, "year"] <- 2019
 
 
 #Mexico: 2019 data not available -> use 2018 data
-missing_mexico <- data
+missing_mexico <- revenue
 missing_mexico <- subset(missing_mexico, subset = iso_3 == "MEX" & year == "2018")
 missing_mexico[missing_mexico$year == 2018, "year"] <- 2019
 
 #Slovakia: 2018 data not available for Sales-> use 2017 data for Sales
-missing_slovakia <- data
+missing_slovakia <- revenue
 missing_slovakia <- subset(missing_slovakia, subset = iso_3 == "SVK" & year == "2017" & tax == "5112 Sales")
 missing_slovakia[missing_slovakia$year == 2017, "year"] <- 2018
 
 
 #Combine data
-data <- rbind(data, missing_australia,missing_greece_vat,missing_greece_excise,
+revenue <- rbind(revenue, missing_australia,missing_greece_vat,missing_greece_excise,
               missing_japan_sales_2017,missing_japan_sales_2018,missing_japan_sales_2019,
               missing_japan_2019,missing_mexico,missing_slovakia)
 
 #Sort dataset
-data <- data[order(data$country, data$tax, data$year),]
+revenue <- revenue[order(revenue$country, revenue$tax, revenue$year),]
 
-write.csv(data, paste(revenue,"revenue_fixed.csv",sep=""), row.names = F)
+write.csv(revenue, paste(revenues,"revenue_fixed.csv",sep=""), row.names = F)
+
+#Spread into wide dataset####
+#Percent of total tax revenue
+revenue_per_tax<-subset(revenue,revenue$variable=="% of Total Revenue")
 
 
-#Means for 2000-2019####
+revenue_per_tax_wide<-revenue_per_tax%>%
+  spread(tax,value)
+names(revenue_per_tax_wide)
+
+colnames(revenue_per_tax_wide)<-c("iso_3" ,"variable" ,"year",
+                                  "country"  ,"iso_2","5000_consumption_pct_total",
+                                  "5111_vat_pct_total","5112_sales_pct_total","5121_excise_pct_total")
+
+revenue_per_tax_wide<-subset(revenue_per_tax_wide,select=-c(variable))
+
+#Percent of GDP
+revenue_per_gdp<-subset(revenue,revenue$variable=="% of GDP")
+
+
+revenue_per_gdp_wide<-revenue_per_gdp%>%
+  spread(tax,value)
+names(revenue_per_gdp_wide)
+
+colnames(revenue_per_gdp_wide)<-c("iso_3" ,"variable" ,"year",
+                                  "country"  ,"iso_2","5000_consumption_pct_gdp",
+                                  "5111_vat_pct_gdp","5112_sales_pct_gdp","5121_excise_pct_gdp")
+
+revenue_per_gdp_wide<-subset(revenue_per_gdp_wide,select=-c(variable))
+
+#Merge
+revenue_wide<-merge(revenue_per_tax_wide,revenue_per_gdp_wide,by=c("iso_3","iso_2","country","year"))
+write.csv(revenue_wide,paste(revenues,"revenue_final.csv",sep=""),row.names = F)
+
+#Means####
 magic_for(silent = TRUE)
-years<-print(unique(data$year))
+years<-print(unique(revenue$year))
 
+#Average pct of total revenues
 for(year in years){
-  consumption_pct_rev<-mean(data$value[data$year==year&
-                                         data$tax=="5000 Consumption"&
-                                         data$variable=="% of Total Revenue"])
-  vat_pct_rev<-mean(data$value[data$year==year&
-                                         data$tax=="5111 VAT"&
-                                         data$variable=="% of Total Revenue"])
-  sales_pct_rev<-mean(data$value[data$year==year&
-                                 data$tax=="5112 Sales"&
-                                 data$variable=="% of Total Revenue"])
-  excise_pct_rev<-mean(data$value[data$year==year&
-                                   data$tax=="5121 Excise"&
-                                   data$variable=="% of Total Revenue"])
+  consumption_pct_rev<-mean(revenue$value[revenue$year==year&
+                                         revenue$tax=="5000 Consumption"&
+                                         revenue$variable=="% of Total Revenue"])
+  vat_pct_rev<-mean(revenue$value[revenue$year==year&
+                                         revenue$tax=="5111 VAT"&
+                                         revenue$variable=="% of Total Revenue"])
+  sales_pct_rev<-mean(revenue$value[revenue$year==year&
+                                 revenue$tax=="5112 Sales"&
+                                 revenue$variable=="% of Total Revenue"])
+  excise_pct_rev<-mean(revenue$value[revenue$year==year&
+                                   revenue$tax=="5121 Excise"&
+                                   revenue$variable=="% of Total Revenue"])
 
   put(consumption_pct_rev,vat_pct_rev,sales_pct_rev,excise_pct_rev)
 }
 pct_rev<-magic_result_as_dataframe() 
 
-write.csv(pct_rev, paste(revenue,"consumption_pct_rev.csv",sep=""), row.names = F)
+#Add number of countries by year
+revenue_wide$key<-if_else(revenue_wide$country!="NA",1,0)
+count<-revenue_wide%>%
+  count(key,year)
 
+count<-subset(count,count$key!="NA")
 
+pct_rev<-merge(pct_rev,count,by="year")
+pct_rev<-pct_rev[-c(6)]
+colnames(pct_rev)[colnames(pct_rev)=="n"] <- "observations"
+
+write.csv(pct_rev, paste(revenues,"consumption_pct_rev.csv",sep=""), row.names = F)
+
+#Average pct of GDP
 for(year in years){
-  consumption_pct_gdp<-mean(data$value[data$year==year&
-                                         data$tax=="5000 Consumption"&
-                                         data$variable=="% of GDP"])
-  vat_pct_gdp<-mean(data$value[data$year==year&
-                                 data$tax=="5111 VAT"&
-                                 data$variable=="% of GDP"])
-  sales_pct_gdp<-mean(data$value[data$year==year&
-                                   data$tax=="5112 Sales"&
-                                   data$variable=="% of GDP"])
-  excise_pct_gdp<-mean(data$value[data$year==year&
-                                    data$tax=="5121 Excise"&
-                                    data$variable=="% of GDP"])
+  consumption_pct_gdp<-mean(revenue$value[revenue$year==year&
+                                         revenue$tax=="5000 Consumption"&
+                                         revenue$variable=="% of GDP"])
+  vat_pct_gdp<-mean(revenue$value[revenue$year==year&
+                                 revenue$tax=="5111 VAT"&
+                                 revenue$variable=="% of GDP"])
+  sales_pct_gdp<-mean(revenue$value[revenue$year==year&
+                                   revenue$tax=="5112 Sales"&
+                                   revenue$variable=="% of GDP"])
+  excise_pct_gdp<-mean(revenue$value[revenue$year==year&
+                                    revenue$tax=="5121 Excise"&
+                                    revenue$variable=="% of GDP"])
   
   put(consumption_pct_gdp,vat_pct_gdp,sales_pct_gdp,excise_pct_gdp)
 }
 pct_gdp<-magic_result_as_dataframe() 
 
-write.csv(pct_gdp, paste(revenue,"consumption_pct_gdp.csv",sep=""), row.names = F)
+#Add number of countries by year
+pct_gdp<-merge(pct_gdp,count,by="year")
+pct_gdp<-pct_gdp[-c(6)]
+colnames(pct_gdp)[colnames(pct_gdp)=="n"] <- "observations"
+
+write.csv(pct_gdp, paste(revenues,"consumption_pct_gdp.csv",sep=""), row.names = F)
